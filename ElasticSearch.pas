500ef497421f20bc8a96774c82754f6b4c07f28c
Gnoztis <53469772+Gnoztis@users.noreply.github.com>
2021-09-15 10:19:45 +0300

    release
    First release

---
+++ ElasticSearch.pas
@@ -0,0 +1,250 @@
+unit ElasticSearch;
+
+interface
+
+uses
+  System.Net.URLClient, System.Net.HttpClient, System.Net.HttpClientComponent,
+  System.Classes, System.SysUtils,
+  json;
+
+
+  type
+    tversion = record
+       numder: string;
+       major : Byte;
+    end;
+
+  type
+    TElasticCLient = class
+    private
+      NetHTTPClient: TNetHTTPClient;
+      BaseUrl:String;
+      version: tversion;
+    public
+      host: string;
+      port: word;
+
+      constructor Create;
+      destructor Destroy; override;
+
+      function connect:boolean;
+      function get_version:string;
+      //index
+      function CreateIndex(IndexName:String):IHTTPResponse; overload;
+      function CreateIndex(IndexName:String; template:string ):IHTTPResponse; overload;
+      function DeleteIndex(IndexName:String):IHTTPResponse;
+
+      //template
+      function CreateTemplate(template:String):IHTTPResponse;
+      //task
+      function _task: IHTTPResponse; overload;
+      function _task(task_id: string): IHTTPResponse; overload;
+      //search
+      function _search(body:string): string; overload;
+      function _search(body:string; index:string): string; overload;
+
+      function _searchEX(reqest:string): IHTTPResponse;
+       //
+      function _bulk(body:string; IndexName:String):IHTTPResponse;
+    end;
+
+function Guid:string;
+
+implementation
+
+function Guid:string;
+var
+  Uid: TGuid;
+  GuidResult: HResult;
+  s:string;
+begin
+result:='00000000-0000-0000-0000-000000000000';
+
+GuidResult := CreateGuid(Uid);
+if GuidResult = S_OK then
+  begin
+    s:=GuidToString(Uid);
+    result:=Copy(s, 2, Length(s) - 2)
+  end;
+end;
+
+constructor TElasticCLient.Create;
+begin
+  NetHTTPClient := TNetHTTPClient.Create(nil);
+  NetHTTPClient.ContentType       :=  'application/json';
+  NetHTTPClient.AcceptCharSet     :=  'UTF-8';
+  NetHTTPClient.UserAgent         :=  'Delphi for ElasticSearch';
+  NetHTTPClient.ConnectionTimeout := 1000;
+  NetHTTPClient.ResponseTimeout   := 60000;
+end;
+
+destructor TElasticCLient.Destroy;
+begin
+  NetHTTPClient.Free;
+  inherited;
+end;
+
+
+function TElasticCLient.connect;
+var
+  JsonObject:TJSONObject;
+  JSonValue:TJSonValue;
+  response: IHTTPResponse;
+begin
+
+ BaseUrl:= 'http://'+host+':'+IntToStr(port)+'/';
+
+ result:=false;
+
+  try
+    response:=NetHTTPClient.get(BaseURL);
+
+    if response.StatusCode = 200 then
+       begin
+          JsonObject:=TJSONObject.Create;
+          try                            //check Elastic Version
+            JsonObject := TJSONObject.ParseJSONValue(response.ContentAsString(TEncoding.UTF8) ) as TJSONObject;
+            if Assigned(JsonObject) then
+               begin
+                 if Assigned(JsonObject.GetValue('version')) then
+                    begin
+                       JSonValue:=TJSonObject.ParseJSONValue(JsonObject.GetValue('version').ToString);
+                       try
+                         version.numder := JSonValue.GetValue<string>('number');
+                         version.major  := StrToInt(version.numder[1]); // rewrite, temporarily
+                         result:=true;
+                       finally
+                         JSonValue.Free;
+                       end;
+
+                    end;
+
+               end;
+          finally
+            JsonObject.Free;
+          end;
+
+       end;
+
+
+  except
+     raise Exception.Create('connection error');
+  end;
+
+  //
+end;
+
+function TElasticCLient._searchEX(reqest:string):IHTTPResponse;
+var
+  JsonToSend:TStringStream;
+begin
+   JsonToSend := TStringStream.Create;
+   JsonToSend.WriteString(reqest);
+
+   try
+     result:= NetHTTPClient.Post(BaseURL, JsonToSend);
+   finally
+    JsonToSend.Free;
+   end;
+
+end;
+
+function TElasticCLient.CreateTemplate(template:String):IHTTPResponse;
+begin
+  result:=NetHTTPClient.POST(BaseURL+'_template/rusiem?pretty',template);
+end;
+
+function TElasticCLient.CreateIndex(IndexName:String):IHTTPResponse;
+begin
+  result:=NetHTTPClient.Put(BaseURL+IndexName);
+end;
+
+function TElasticCLient.CreateIndex(IndexName:String; template:string):IHTTPResponse;
+begin
+  result:=NetHTTPClient.Put(BaseURL+IndexName, template);
+end;
+
+function TElasticCLient.DeleteIndex(IndexName:String):IHTTPResponse;
+begin
+   result:=NetHTTPClient.Delete(BaseURL+IndexName);
+end;
+////////////////////////////////////////////////////////////////////////////////
+function TElasticCLient._task: IHTTPResponse;
+begin
+   result:=NetHTTPClient.get(BaseURL+'_tasks');
+end;
+
+
+function TElasticCLient._task(task_id: string): IHTTPResponse;
+begin
+   result:=NetHTTPClient.get(BaseURL+'_tasks/'+task_id);
+end;
+////////////////////////////////////////////////////////////////////////////////
+function TElasticCLient._search(body:string): string;
+begin
+  if body<>'' then
+      result:= NetHTTPClient.GET(BaseURL+'_search?q='+body).ContentAsString(TEncoding.UTF8)
+  else
+      result:= NetHTTPClient.GET(BaseURL+'_search').ContentAsString(TEncoding.UTF8);
+end;
+
+function TElasticCLient._search(body:string; index:string): string;
+begin
+  if body<>'' then
+    result:= NetHTTPClient.GET(BaseURL+index+'/_search?q='+body).ContentAsString(TEncoding.UTF8)
+  else
+    result:= NetHTTPClient.GET(BaseURL+index+'/_search').ContentAsString(TEncoding.UTF8)
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+function TElasticCLient._bulk(body:string; IndexName:String):IHTTPResponse;
+var
+  Request     : TStringList;
+  Index       : TJSONObject;
+  IndexValue  : TJSONObject;
+  JsonToSend  : TStringStream;
+  i           : Integer;
+
+const
+    CR = #13#10;
+begin
+   Request    := TStringList.Create;
+   Index      := TJSONObject.Create;
+   IndexValue := TJSONObject.Create;
+
+   IndexValue.AddPair('_id', TJSONNull.Create);
+   IndexValue.AddPair('_index', IndexName);
+
+   if version.major < 7 then
+     IndexValue.AddPair('_type', 'doc'); //ElasticSearch 5.6
+
+   Index.AddPair('index', TJSONObject.ParseJSONValue(IndexValue.ToString) );
+
+   JsonToSend := TStringStream.Create;
+
+   try
+      Request.Text:= body;
+
+      for i := 0 to Request.Count-1 do
+          begin
+            JsonToSend.WriteString(Index.ToString+cr);
+            JsonToSend.WriteString(Request.Strings[i]+cr);
+          end;
+
+      JsonToSend.Position:=0;
+      result:= NetHTTPClient.Post(BaseURL+'_bulk', JsonToSend);
+   finally
+     Request.Free;
+     FreeAndNil( Index );
+     JsonToSend.Free;
+   end;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+function TElasticCLient.get_version:string;
+begin
+  result:= version.numder;
+end;
+
+
+end.
---
+++ libXmlToJson.pas
@@ -0,0 +1,114 @@
+﻿unit libXmlToJson;
+interface
+uses
+ xmldoc, 
+ Xml.XMLIntf, 
+ system.json, 
+ system.Variants, 
+ system.VarUtils, 
+ sysutils, 
+ dbxjson,
+ System.Classes;
+
+function xml_to_json(AXML : IXMLNode): string; overload;
+procedure add_attributes_in_jsonObject(AXML : IXMLNode; AJSONTarget : TJSONObject);
+
+implementation
+
+procedure add_attributes_in_jsonObject(AXML : IXMLNode; AJSONTarget : TJSONObject);
+var
+  odata : OleVariant;
+  j: integer;
+begin
+    if AXML.AttributeNodes.Count > 0 then
+    begin
+        for j := 0 to AXML.AttributeNodes.Count -1 do
+        begin
+            odata := AXML.AttributeNodes.Nodes[j].NodeValue;
+            if  VarIsNull(odata) then
+               odata := 'nil';
+
+            AJSONTarget.AddPair('_' + AXML.AttributeNodes.Nodes[j].NodeName, odata);
+        end;
+    end;
+end;
+
+function xml_to_json(AXML : IXMLNode): string;
+var
+  oJS : TJSONObject;
+  oJSListCollection : TJSONArray;
+  i, j : integer;
+  svalue : string;
+
+  s: string;
+begin
+
+   s:=  AXML.xml;
+
+
+    oJS := TJSONObject.Create;
+    
+    add_attributes_in_jsonObject(AXML, oJS);
+
+    for I := 0 to AXML.ChildNodes.Count -1 do
+    begin
+        if AXML.ChildNodes.Nodes[i].Collection <> nil then
+        begin
+             oJSListCollection := TJSONArray.Create;
+             try
+                 for j := 0 to AXML.ChildNodes.Nodes[i].Collection.Count-1 do
+                 begin
+                    svalue := xml_to_json(AXML.ChildNodes.Nodes[i].Collection.Nodes[j]).replace('"{', '{').Replace('}"', '}');
+                    oJSListCollection.Add(svalue);
+                 end;
+                 Result := oJSListCollection.ToJSON;
+             finally
+                 oJSListCollection.Free;
+                 oJS.Free; 
+             end;
+             Exit;
+        end
+        else
+        if (AXML.ChildNodes.Nodes[i].NodeType = ntElement) then
+        begin
+
+            if AXML.ChildNodes.Nodes[i].ChildNodes.Count = 1 then
+            begin
+
+                if (AXML.ChildNodes.Nodes[i].ChildNodes.First.NodeType = ntText) then
+                begin
+                   oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, AXML.ChildNodes.Nodes[i].ChildNodes.First.NodeValue);
+                end
+                else if (AXML.ChildNodes.Nodes[i].ChildNodes.First.NodeType = ntCData) then
+                begin
+                   oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, QuotedStr(AXML.ChildNodes.Nodes[i].ChildNodes.First.Text));
+                end
+                else 
+                if (AXML.ChildNodes.Nodes[i].HasChildNodes) or (AXML.ChildNodes.Nodes[i].AttributeNodes.Count > 0) then
+                    oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, xml_to_json(AXML.ChildNodes.Nodes[i]))
+                else
+                    oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, 'nil');
+            end
+            else 
+            if (AXML.ChildNodes.Nodes[i].HasChildNodes) or (AXML.ChildNodes.Nodes[i].AttributeNodes.Count > 0) then
+              begin
+                oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, xml_to_json(AXML.ChildNodes.Nodes[i]));
+              end
+
+            else
+                oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, 'nil');
+        end
+        else
+        if (AXML.ChildNodes.Nodes[i].NodeType = ntCData) then
+        begin
+           oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, AXML.ChildNodes.Nodes[i].NodeValue);
+
+           // oJS.AddPair(AXML.ChildNodes.Nodes[i].NodeName, QuotedStr(AXML.ChildNodes.Nodes[i].ChildNodes.First.Text));
+
+        end;
+    end;
+
+    Result := oJS.ToJSON.Replace('\', '').Replace('"[', '[').Replace(']"', ']').replace('"{', '{').Replace('}"', '}');
+    oJS.Free;
+end;
+end.
